fe12ac9b076e956fba20fdfda08a1e8102777e56
Huy Tyler <huytyler15@gmail.com>
2024-06-18 19:50:56 +0700

    Expo Router Screens

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,9 +3,9 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/src/components/useColorScheme';
-import { useClientOnlyValue } from '@/src/components/useClientOnlyValue';
+import Colors from '../../constants/Colors';
+import { useColorScheme } from 'react-native';
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -26,11 +26,20 @@ export default function TabLayout() {
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
       }}>
+      
       <Tabs.Screen
         name="index"
+        options={{ 
+          href:null,
+        }}
+      />
+
+      <Tabs.Screen
+        name="menu"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Menu for you today!',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="cutlery" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -50,8 +59,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Order what?',
+          tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
         }}
       />
     </Tabs>
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,15 +1,28 @@
-import { StyleSheet, Text, View, Image  } from 'react-native';
-import Colors from '@/constants/Colors';
+import { StyleSheet, Text, View, Image, Pressable  } from 'react-native';
+import Colors from '../constants/Colors';
+import { Product } from '../types';
+import { Link } from 'expo-router';
 
-const ProductListItem = ({ product })=>{ 
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+  product : Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps)=>{ 
   return(
-    <View style={styles.container}>
-    <Image source = {{uri: product.image}} style={styles.image} />
- 
+    <Link href={`/${product.id}`} asChild>
+       <Pressable  style={styles.container}>    
+        <Image 
+            source = {{uri: product.image || defaultPizzaImage}} 
+            style={styles.image}
+            resizeMode='contain'
+         />
 
-    <Text style={styles.title}>{product.name}</Text>
-    <Text style={styles.price}>${product.price}</Text>
-    </View>
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+        </Pressable>
+    </Link>
   );
 }
 
@@ -20,6 +33,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding:10,
     borderRadius: 20,
+    flex: 1,
+
   },
   image:{
     width: '100%',
