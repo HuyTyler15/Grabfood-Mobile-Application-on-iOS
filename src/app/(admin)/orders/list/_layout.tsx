e621864967b62f7f024d7b2eef7f5a3e8aea0640
Huy Tyler <huytyler15@gmail.com>
2024-06-29 06:59:01 +0700

    Add the react-material-top-tabs to Order/Admin side

--- src/app/(admin)/orders/list/_layout.tsx
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -13,7 +13,5 @@ export default function OrderListNavigator(){
                 <TopTabs.Screen name ="index" options={{ title: 'Active' }} />
             </TopTabs>
         </SafeAreaView>
-
     );
-
 }
