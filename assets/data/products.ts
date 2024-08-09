c8973d21abc1d14db7658cb2af90909d6f7fbe51
Huy Tyler <huytyler15@gmail.com>
2024-08-10 06:20:59 +0700

    Fixing UI

--- assets/data/products.ts
+++ assets/data/products.ts
@@ -1,74 +1,73 @@
 const products = [
   {
     id: 1,
-    name: 'Ultimate Pepperoni',
+    name: "Pepperoni Pizza",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+      "https://www.simplyrecipes.com/thmb/2MQuChhZANaSSxdL1a0tA6nBgmQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg",
     price: 12.99,
   },
   {
     id: 2,
-    name: 'Extravaganzza',
+    name: "Cheese & Tomato Pizza",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+      "https://www.myactivekitchen.com/wp-content/uploads/2016/02/Cheese-and-tomato-pizza_image_5.jpg",
     price: 14.99,
   },
   {
     id: 3,
-    name: 'Meat Pizza',
+    name: "Meat Pizza",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-      //'https://www.southernliving.com/thmb/j_6gABRIAMegN6RFHxOgbUqBxjA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/2652401_QFSSL_SupremePizza_00072-d910a935ba7d448e8c7545a963ed7101.jpg',
-    price: 13.47,
+      "https://www.effortlessfoodie.com/wp-content/uploads/2022/02/meat-feast-pizza-6.jpg",
+    price: 15.29,
   },
   {
     id: 4,
-    name: 'Margarita',
+    name: "Margherita Pizza ",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
-    price: 9.9,
+      "https://www.acouplecooks.com/wp-content/uploads/2022/10/Margherita-Pizza-082.jpg",
+    price: 22.99,
   },
   {
     id: 5,
-    name: 'Pacific Veggie',
+    name: "Ultimate Veggie Pizza",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.99,
+      "https://cookieandkate.com/images/2020/10/best-veggie-pizza-recipe-1-550x824.jpg",
+    price: 19.99,
   },
   {
     id: 6,
-    name: 'Hawaiian',
+    name: "Hawaiian Pizza",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
-    price: 10.49,
+      "https://recipes.net/wp-content/uploads/2020/03/dominos-copycat-hawaiian-pizza-recipe.jpg",
+    price: 15.99,
   },
   {
     id: 7,
-    name: 'Deluxe',
+    name: "Deluxe Pizza",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
-    price: 16.99,
+      "https://i0.wp.com/waynesburgpizza.com/wp-content/uploads/2019/12/Deluxe-1-scaled.jpg?fit=2560%2C1920&ssl=1",
+    price: 12.99,
   },
   {
     id: 8,
-    name: 'BBQ Chicken',
+    name: "BBQ Chicken Pizza",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.89,
+      "https://www.southernliving.com/thmb/wV7YfCLKfWBTdRAYY679xatR_2E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/BBQ-Chicken-Pizza_Audit7102_Beauty_1_3x2-e8195f3f45c74f189742d6eb4d7b2116.jpg",
+    price: 20.89,
   },
   {
     id: 9,
-    name: 'Chicken Bacon Ranch',
+    name: "Chicken Bacon Ranch Pizza",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: 13.99,
+      "https://i0.wp.com/weirdandwildpizza.com/wp-content/uploads/2021/01/chicken-bacon-ranch-3.jpg?resize=740%2C416&ssl=1",
+    price: 18.99,
   },
   {
     id: 10,
-    name: 'Cheese',
+    name: "Neapolitan Pizza",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
-    price: 13.29,
+      "https://au.ooni.com/cdn/shop/articles/OG_Margherita_454eb0a2-95cc-4e63-afa3-159f342788c6.jpg?v=1662487615&width=2048",
+    price: 15.29,
   },
 ];
 
